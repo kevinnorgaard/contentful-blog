68227bcef1a89ccbf9e0dc7edff37f6d63dff3cc
Kevin Norgaard <norgaark@uci.edu>
2020-04-05 23:16:35 -0700

    Added style & wellness og meta

--- src/app/pages/wellness/wellness.component.ts
+++ src/app/pages/wellness/wellness.component.ts
@@ -4,6 +4,7 @@ import { switchMap } from 'rxjs/operators';
 import { ContentfulService } from 'src/app/contentful.service';
 import { empty, from } from 'rxjs';
 import { Entry } from 'contentful';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-wellness',
@@ -13,7 +14,20 @@ import { Entry } from 'contentful';
 export class WellnessComponent implements OnInit {
   category = 'wellness';
 
-  constructor() { }
+  constructor(private metaService: Meta) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateOgTags();
+  }
+
+  updateOgTags() {
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({ property: 'og:image', content: 'https://universal-4zfyqvma3q-uw.a.run.app/assets/wellness.jpg' });
+    this.metaService.addTag({ property: 'og:title', content: 'Wellness' });
+    this.metaService.addTag({ property: 'og:description', content: 'Wellness description goes here' });
+    this.metaService.addTag({ name: 'twitter:title', content: 'Wellness' });
+    this.metaService.addTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.metaService.addTag({ name: 'twitter:description', content: 'Wellness description goes here' });
+    this.metaService.addTag({ name: 'twitter:image', content: 'https://universal-4zfyqvma3q-uw.a.run.app/assets/wellness.jpg' });
+  }
 }
