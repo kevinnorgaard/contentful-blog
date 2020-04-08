c99b0603ebbfb8b34929ecf1668be16d479d2920
Kevin Norgaard <norgaark@uci.edu>
2020-04-08 10:42:55 -0700

    Minor fix

--- src/app/pages/style-beauty/style-beauty.component.ts
+++ src/app/pages/style-beauty/style-beauty.component.ts
@@ -9,8 +9,21 @@ import { Meta } from '@angular/platform-browser';
 export class StyleBeautyComponent implements OnInit {
   category = 'style-beauty';
 
-  constructor() {
+  constructor(private metaService: Meta) {
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateOgTags();
+  }
+
+  updateOgTags() {
+    this.metaService.updateTag({ property: 'og:type', content: 'website' });
+    this.metaService.updateTag({ property: 'og:image', content: 'https://universal-4zfyqvma3q-uw.a.run.app/assets/style.jpg' });
+    this.metaService.addTag({ property: 'og:title', content: 'Style & Beauty' });
+    this.metaService.addTag({ property: 'og:description', content: 'S&B description goes here' });
+    this.metaService.addTag({ name: 'twitter:title', content: 'Style & Beauty' });
+    this.metaService.addTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.metaService.addTag({ name: 'twitter:description', content: 'S&B description goes here' });
+    this.metaService.addTag({ name: 'twitter:image', content: 'https://universal-4zfyqvma3q-uw.a.run.app/assets/style.jpg' });
+  }
 }
